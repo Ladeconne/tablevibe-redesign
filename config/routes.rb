c9eae90bc442c3eaaa2fa822e1a965b56851c1dc
Yves Hurdequint <hurdequinty@hotmail.com>
2021-09-01 11:56:27 +0800

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/pricing', to: 'pages#pricing', as: :pricing
+  get '/how-it-works', to: 'pages#how_it_works', as: :how_it_works
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
